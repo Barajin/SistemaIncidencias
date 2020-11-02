675d9cc0b3a69b10fdb22be4b342ee03214c1bd9
Jesús Barajas Villegas <barajin21@gmail.com>
2020-11-02 12:45:52 -0700

    Dev jaz (#4)
    * alta personas
    
    * pantalla personal-tabla
    
    Co-authored-by: luisesh1x10 <luisesh1x10>

---
+++ Sistema_Incidencias/Sistema_Incidencias/Personal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Incidencias
+{
+    public partial class Personal : Form
+    {
+        public Personal()
+        {
+            InitializeComponent();
+        }
+
+        private void tablaPersonalDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
