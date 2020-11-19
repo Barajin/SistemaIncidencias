3fae2df1b93a63876eec1a7b4f679f028a59fd7b
Oscar <zureichenden@gmail.com>
2020-11-19 01:55:42 -0700

    Aceptar o rechazar incidencias
    Se cambia el estado con un UPDATE en SQL

--- Sistema_Incidencias/Sistema_Incidencias/FormIncidenciasJefeTaller.cs
+++ Sistema_Incidencias/Sistema_Incidencias/FormIncidenciasJefeTaller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Cache;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,8 @@ namespace Sistema_Incidencias
 {
     public partial class FormIncidenciasJefeTaller : Form
     {
+        int incidenciaID = 0;
+
         public FormIncidenciasJefeTaller()
         {
             InitializeComponent();
@@ -26,23 +29,87 @@ namespace Sistema_Incidencias
 
         private void dgvIncidencias_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            incidenciaID = Convert.ToInt32(dgvIncidencias.CurrentRow.Cells[0].Value.ToString());
+
             if (e.ColumnIndex == dgvIncidencias.Columns["aceptar"].Index)
             {
-                MessageBox.Show("Incidencia aceptada.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                incidenciaID = Convert.ToInt32(dgvIncidencias.CurrentRow.Cells[0].Value.ToString());
 
+                var connetionString = "Server=.\\SQLEXPRESS; Database= Sistema_Incidencias; Integrated Security=True";
+                var sql = "UPDATE incidencia SET estado = 2 where incidencia.id = @id";// repeat for all variables
+               
+                    using (var connection = new SqlConnection(connetionString))
+                    {
+                        using (var command = new SqlCommand(sql, connection))
+                        {
+                            command.Parameters.Add("@id", incidenciaID);
+                          
+                            connection.Open();
+                            int result = command.ExecuteNonQuery();
+
+                        if (result < 0)
+                            Console.WriteLine("Error inserting data into Database!");
+                        else
+                        {
+                            MessageBox.Show("Incidencia aceptada.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                
             }
             else if (e.ColumnIndex == dgvIncidencias.Columns["rechazar"].Index)
             {
-                MessageBox.Show("Incidencia rechazada.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                incidenciaID = Convert.ToInt32(dgvIncidencias.CurrentRow.Cells[0].Value.ToString());
+
+                var connetionString = "Server=.\\SQLEXPRESS; Database= Sistema_Incidencias; Integrated Security=True";
+                var sql = "UPDATE incidencia SET estado = 3 where incidencia.id = @id";// repeat for all variables
+
+                using (var connection = new SqlConnection(connetionString))
+                {
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.Add("@id", incidenciaID);
+
+                        connection.Open();
+                        int result = command.ExecuteNonQuery();
+
+                        if (result < 0)
+                            Console.WriteLine("Error inserting data into Database!");
+                        else
+                        {
+                            MessageBox.Show("Incidencia rechazada.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
 
             }
         }
 
         public void llenarTabla()
         {
-
             var select = "select i.titulo, i.descripcion, i.prioridad, p.nombre  +' '+ p.apellidoPaterno as 'Jefe Departamento', ti.nombre as Tipo  from incidencia i" +
-                " join persona p on i.persona = p.id join tipos_incidencia ti on i.tipo = ti.id";
+               " join persona p on i.persona = p.id join tipos_incidencia ti on i.tipo = ti.id";
+
+
+            if (UserLoginCache.Cargo == "Jefe de Taller de Hardware")
+            {
+                select = "select i.id, i.titulo, i.descripcion, i.prioridad, p.nombre  +' '+ p.apellidoPaterno as 'Jefe Departamento', ti.nombre as Tipo  from incidencia i" +
+               " join persona p on i.persona = p.id join tipos_incidencia ti on i.tipo = ti.id where ti.nombre = 'Hardware' and i.estado =1";
+            }
+
+            else if(UserLoginCache.Cargo == "Jefe de Taller de Software")
+            {
+                select = "select i.id, i.titulo, i.descripcion, i.prioridad, p.nombre  +' '+ p.apellidoPaterno as 'Jefe Departamento', ti.nombre as Tipo  from incidencia i" +
+                " join persona p on i.persona = p.id join tipos_incidencia ti on i.tipo = ti.id where ti.nombre = 'Software' and i.estado =1";
+            }
+
+            else if (UserLoginCache.Cargo == "Jefe de Taller de Redes")
+            {
+                select = "select i.id, i.titulo, i.descripcion, i.prioridad, p.nombre  +' '+ p.apellidoPaterno as 'Jefe Departamento', ti.nombre as Tipo  from incidencia i" +
+                " join persona p on i.persona = p.id join tipos_incidencia ti on i.tipo = ti.id where ti.nombre = 'Redes' and i.estado =1";
+            }
+
+
 
             var comando = new SqlConnection("Server=.\\SQLEXPRESS; Database= Sistema_Incidencias; Integrated Security=True"); // Your Connection String here
             var dataAdapter = new SqlDataAdapter(select, comando);
