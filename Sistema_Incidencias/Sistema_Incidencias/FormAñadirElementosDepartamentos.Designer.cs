659ad67c9a441ead92911a5c7176bbeb5182141f
danicordova <danicordovag@gmail.com>
2020-12-07 12:06:01 -0700

    Se solucionó el error al asignar elemento de TI con departamento

--- Sistema_Incidencias/Sistema_Incidencias/FormAñadirElementosDepartamentos.Designer.cs
+++ Sistema_Incidencias/Sistema_Incidencias/FormAñadirElementosDepartamentos.Designer.cs
@@ -29,135 +29,95 @@ namespace Sistema_Incidencias
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label6 = new System.Windows.Forms.Label();
+            this.cmb_departamento = new System.Windows.Forms.ComboBox();
+            this.btn_consultar = new System.Windows.Forms.Button();
+            this.txt_modelo = new System.Windows.Forms.TextBox();
+            this.txt_marca = new System.Windows.Forms.TextBox();
             this.btnAgregarDepartamento = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_descripcion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmboTipoElemento = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(73, 135);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(167, 24);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 110);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Elemento";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 192);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Marca";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(73, 218);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(167, 24);
-            this.comboBox2.TabIndex = 2;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(264, 192);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Modelo";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(73, 295);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(389, 22);
-            this.textBox1.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(69, 265);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 17);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Descripcion";
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(73, 368);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown1.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(69, 336);
+            this.label5.Location = new System.Drawing.Point(37, 329);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Garantia";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Departamento";
             // 
-            // dateTimePicker1
+            // cmb_departamento
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(261, 366);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 10;
+            this.cmb_departamento.Enabled = false;
+            this.cmb_departamento.FormattingEnabled = true;
+            this.cmb_departamento.Location = new System.Drawing.Point(40, 344);
+            this.cmb_departamento.Margin = new System.Windows.Forms.Padding(2);
+            this.cmb_departamento.Name = "cmb_departamento";
+            this.cmb_departamento.Size = new System.Drawing.Size(175, 21);
+            this.cmb_departamento.TabIndex = 32;
             // 
-            // label6
+            // btn_consultar
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(259, 336);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(118, 17);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Fecha de compra";
+            this.btn_consultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn_consultar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_consultar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btn_consultar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_consultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_consultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_consultar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_consultar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_consultar.Location = new System.Drawing.Point(208, 83);
+            this.btn_consultar.Name = "btn_consultar";
+            this.btn_consultar.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btn_consultar.Size = new System.Drawing.Size(99, 31);
+            this.btn_consultar.TabIndex = 31;
+            this.btn_consultar.Text = "Consultar";
+            this.btn_consultar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_consultar.UseVisualStyleBackColor = false;
+            this.btn_consultar.Click += new System.EventHandler(this.btn_consultar_Click);
+            // 
+            // txt_modelo
+            // 
+            this.txt_modelo.Enabled = false;
+            this.txt_modelo.Location = new System.Drawing.Point(208, 219);
+            this.txt_modelo.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_modelo.Name = "txt_modelo";
+            this.txt_modelo.Size = new System.Drawing.Size(126, 20);
+            this.txt_modelo.TabIndex = 30;
+            // 
+            // txt_marca
+            // 
+            this.txt_marca.Enabled = false;
+            this.txt_marca.Location = new System.Drawing.Point(40, 219);
+            this.txt_marca.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_marca.Name = "txt_marca";
+            this.txt_marca.Size = new System.Drawing.Size(126, 20);
+            this.txt_marca.TabIndex = 29;
             // 
             // btnAgregarDepartamento
             // 
             this.btnAgregarDepartamento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.btnAgregarDepartamento.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnAgregarDepartamento.Enabled = false;
             this.btnAgregarDepartamento.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
             this.btnAgregarDepartamento.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.btnAgregarDepartamento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAgregarDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgregarDepartamento.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.btnAgregarDepartamento.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAgregarDepartamento.Location = new System.Drawing.Point(73, 450);
-            this.btnAgregarDepartamento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregarDepartamento.Location = new System.Drawing.Point(40, 408);
             this.btnAgregarDepartamento.Name = "btnAgregarDepartamento";
-            this.btnAgregarDepartamento.Padding = new System.Windows.Forms.Padding(7, 0, 0, 0);
-            this.btnAgregarDepartamento.Size = new System.Drawing.Size(132, 38);
-            this.btnAgregarDepartamento.TabIndex = 12;
+            this.btnAgregarDepartamento.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnAgregarDepartamento.Size = new System.Drawing.Size(99, 31);
+            this.btnAgregarDepartamento.TabIndex = 27;
             this.btnAgregarDepartamento.Text = "Guardar";
             this.btnAgregarDepartamento.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnAgregarDepartamento.UseVisualStyleBackColor = false;
@@ -173,49 +133,98 @@ namespace Sistema_Incidencias
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCancelar.Location = new System.Drawing.Point(331, 450);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancelar.Location = new System.Drawing.Point(233, 408);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Padding = new System.Windows.Forms.Padding(7, 0, 0, 0);
-            this.btnCancelar.Size = new System.Drawing.Size(131, 42);
-            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnCancelar.Size = new System.Drawing.Size(98, 34);
+            this.btnCancelar.TabIndex = 28;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.Location = new System.Drawing.Point(267, 218);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(194, 22);
-            this.textBox2.TabIndex = 14;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 257);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Descripcion";
+            // 
+            // txt_descripcion
+            // 
+            this.txt_descripcion.Enabled = false;
+            this.txt_descripcion.Location = new System.Drawing.Point(40, 282);
+            this.txt_descripcion.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_descripcion.Name = "txt_descripcion";
+            this.txt_descripcion.Size = new System.Drawing.Size(293, 20);
+            this.txt_descripcion.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 198);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Marca";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Elemento";
+            // 
+            // CmboTipoElemento
+            // 
+            this.CmboTipoElemento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmboTipoElemento.FormattingEnabled = true;
+            this.CmboTipoElemento.Location = new System.Drawing.Point(40, 89);
+            this.CmboTipoElemento.Margin = new System.Windows.Forms.Padding(2);
+            this.CmboTipoElemento.Name = "CmboTipoElemento";
+            this.CmboTipoElemento.Size = new System.Drawing.Size(126, 21);
+            this.CmboTipoElemento.TabIndex = 21;
             // 
             // FormAñadirElementosDepartamentos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(668, 631);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(380, 513);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmb_departamento);
+            this.Controls.Add(this.btn_consultar);
+            this.Controls.Add(this.txt_modelo);
+            this.Controls.Add(this.txt_marca);
             this.Controls.Add(this.btnAgregarDepartamento);
             this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_descripcion);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Controls.Add(this.CmboTipoElemento);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormAñadirElementosDepartamentos";
             this.Text = "FormAñadirElementosDepartamentos";
             this.Load += new System.EventHandler(this.FormAñadirElementosDepartamentos_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,19 +232,18 @@ namespace Sistema_Incidencias
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmb_departamento;
+        private System.Windows.Forms.Button btn_consultar;
+        private System.Windows.Forms.TextBox txt_modelo;
+        private System.Windows.Forms.TextBox txt_marca;
         private System.Windows.Forms.Button btnAgregarDepartamento;
         private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_descripcion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmboTipoElemento;
     }
 }
--- Sistema_Incidencias/Sistema_Incidencias/FormAñadirElementosDepartamentos.cs
+++ Sistema_Incidencias/Sistema_Incidencias/FormAñadirElementosDepartamentos.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections;
-using System.Data.SqlClient;
-using System.Drawing;
 
 namespace Sistema_Incidencias
 {
     public partial class FormAñadirElementosDepartamentos : Form
     {
+        String tipoElementoTmp = "";
+        DataTable dt2;
+        DataTable dt3;
+        String tmp1 = "";
+        String tmp2 = "";
+
         string connString = "Server=.\\SQLEXPRESS; Database= Sistema_Incidencias; Integrated Security=True";
         public FormAñadirElementosDepartamentos()
         {
@@ -23,18 +27,14 @@ namespace Sistema_Incidencias
 
         private void FormAñadirElementosDepartamentos_Load(object sender, EventArgs e)
         {
-            DataTable dt = ObtenereElemento();
-            comboBox1.DataSource = dt;
-            comboBox1.ValueMember = "id";
-            comboBox1.DisplayMember = "nombre";
+            DataTable dt = CategoryAll();
+            CmboTipoElemento.DataSource = dt;
+            CmboTipoElemento.ValueMember = "id";
+            CmboTipoElemento.DisplayMember = "id";
 
-            DataTable dt2 = ObtenereMarca();
-            comboBox2.DataSource = dt;
-            comboBox2.ValueMember = "marca";
-            comboBox2.DisplayMember = "marca";
         }
 
-        public DataTable ObtenereElemento()
+        public DataTable CategoryAll()
         {
 
             using (var cn = new SqlConnection(connString))
@@ -43,7 +43,7 @@ namespace Sistema_Incidencias
                 {
                     using (var cmd = cn.CreateCommand())
                     {
-                        cmd.CommandText = "select * from tipos_elementoTI";
+                        cmd.CommandText = "Select * from elementoTI where id not in (Select fk_elementoTI from elementoTI_departamento);";
                         da.SelectCommand = cmd;
                         var dt = new DataTable();
                         da.Fill(dt);
@@ -53,8 +53,7 @@ namespace Sistema_Incidencias
             }
         }
 
-
-        public DataTable ObtenereMarca()
+        public DataTable CategoryAll2()
         {
 
             using (var cn = new SqlConnection(connString))
@@ -63,7 +62,7 @@ namespace Sistema_Incidencias
                 {
                     using (var cmd = cn.CreateCommand())
                     {
-                        cmd.CommandText = "select * from elementoTI";
+                        cmd.CommandText = "Select id, tipo, marca, modelo, Descripcion from elementoTI where id = " + tipoElementoTmp;
                         da.SelectCommand = cmd;
                         var dt = new DataTable();
                         da.Fill(dt);
@@ -71,11 +70,106 @@ namespace Sistema_Incidencias
                     }
                 }
             }
+        }
+
+        public DataTable CategoryAll3()
+        {
+
+            using (var cn = new SqlConnection(connString))
+            {
+                using (var da = new SqlDataAdapter())
+                {
+                    using (var cmd = cn.CreateCommand())
+                    {
+                        cmd.CommandText = "select id from departamento;";
+                        da.SelectCommand = cmd;
+                        var dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        public void Incersion()
+        {
+            string connString = "Server=.\\SQLEXPRESS; Database= Sistema_Incidencias; Integrated Security=True";
+
+
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                String query = "INSERT INTO elementoTI_departamento " +
+                    "VALUES (@fk_elementoTi, @fkDepartamento)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@fkDepartamento", tmp2);
+                    command.Parameters.AddWithValue("@fk_elementoTi", tmp1);
+
+
+
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+
+                    // Check Error
+                    if (result < 0)
+                        Console.WriteLine("Error inserting data into Database!");
+                    else
+                        MessageBox.Show(("Se asignó correctamente el departamento al Elemento de TI"), "Asignación de elemento de TI correcta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            this.tmp1 = CmboTipoElemento.Text;
+
+            if (CmboTipoElemento.Text == "")
+            {
+                MessageBox.Show(("Epale está vacio"), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                this.tipoElementoTmp = CmboTipoElemento.Text;
+                dt2 = CategoryAll2();
+
+                txt_marca.Text = dt2.Rows[0].Field<String>(2);
+                txt_modelo.Text = dt2.Rows[0].Field<String>(3);
+                txt_descripcion.Text = dt2.Rows[0].Field<String>(4);
+                cmb_departamento.Enabled = true;
+
+                dt3 = CategoryAll3();
+                cmb_departamento.DataSource = dt3;
+                cmb_departamento.ValueMember = "id";
+                cmb_departamento.DisplayMember = "id";
+                btnAgregarDepartamento.Enabled = true;
+            }
+
         }
 
         private void btnAgregarDepartamento_Click(object sender, EventArgs e)
         {
+            this.tmp2 = cmb_departamento.Text;
 
+            Incersion();
         }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
     }
 }
