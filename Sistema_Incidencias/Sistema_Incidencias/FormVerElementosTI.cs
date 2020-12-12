a70a199b18f40ffc8c37d961616a99cf478751b6
Oscar <zureichenden@gmail.com>
2020-12-11 20:02:40 -0700

    Consultas sql ubicación

--- Sistema_Incidencias/Sistema_Incidencias/FormVerElementosTI.cs
+++ Sistema_Incidencias/Sistema_Incidencias/FormVerElementosTI.cs
@@ -20,7 +20,7 @@ namespace Sistema_Incidencias
 
         private void FormVerElementosTI_Load(object sender, EventArgs e)
         {
-            var select = "Select elementoTI.id as 'Código', tipos_elementoTI.nombre as 'Tipo de Elemento', elementoTI.marca as 'Marca', elementoTI.modelo as 'Modelo', elementoTI.Descripcion as 'Descripción', elementoTI.garantia as 'Años de Garantía', elementoTI.fechaCompra as 'Fecha de Compra', departamento.nombre as 'Departamento Asignado', persona.nombre as 'Nombre de Encargado', persona.apellidoPaterno as 'Apellido' "+
+            var select = "Select elementoTI.id as 'Código', tipos_elementoTI.nombre as 'Tipo de Elemento', elementoTI.ubicacion as 'Ubicación', elementoTI.marca as 'Marca', elementoTI.modelo as 'Modelo', elementoTI.Descripcion as 'Descripción', elementoTI.garantia as 'Años de Garantía', elementoTI.fechaCompra as 'Fecha de Compra', departamento.nombre as 'Departamento Asignado', persona.nombre as 'Nombre de Encargado', persona.apellidoPaterno as 'Apellido' "+
            "From elementoTI Inner Join elementoTI_departamento On elementoTI_departamento.fk_elementoTI = elementoTI.id " +
            "Inner Join departamento On departamento.id = elementoTI_departamento.fk_departamento " +
            "Inner Join persona_elementoTI on persona_elementoTI.fk_elementoTI = elementoTI.id " +
@@ -50,12 +50,12 @@ namespace Sistema_Incidencias
 
         private void FormVerElementosTI_Activated(object sender, EventArgs e)
         {
-            var select = "Select elementoTI.id as 'Código', tipos_elementoTI.nombre as 'Tipo de Elemento', elementoTI.marca as 'Marca', elementoTI.modelo as 'Modelo', elementoTI.Descripcion as 'Descripción', elementoTI.garantia as 'Años de Garantía', elementoTI.fechaCompra as 'Fecha de Compra', departamento.nombre as 'Departamento Asignado', persona.nombre as 'Nombre de Encargado', persona.apellidoPaterno as 'Apellido' " +
-          "From elementoTI Inner Join elementoTI_departamento On elementoTI_departamento.fk_elementoTI = elementoTI.id " +
-          "Inner Join departamento On departamento.id = elementoTI_departamento.fk_departamento " +
-          "Inner Join persona_elementoTI on persona_elementoTI.fk_elementoTI = elementoTI.id " +
-         "Inner Join persona on persona.id = persona_elementoTI.fk_persona " +
-          "Inner Join tipos_elementoTI on elementoTI.tipo = tipos_elementoTI.id Where tipos_elementoTI.id = elementoTI.tipo";
+            var select = "Select elementoTI.id as 'Código', tipos_elementoTI.nombre as 'Tipo de Elemento', elementoTI.ubicacion as 'Ubicación', elementoTI.marca as 'Marca', elementoTI.modelo as 'Modelo', elementoTI.Descripcion as 'Descripción', elementoTI.garantia as 'Años de Garantía', elementoTI.fechaCompra as 'Fecha de Compra', departamento.nombre as 'Departamento Asignado', persona.nombre as 'Nombre de Encargado', persona.apellidoPaterno as 'Apellido' " +
+             "From elementoTI Inner Join elementoTI_departamento On elementoTI_departamento.fk_elementoTI = elementoTI.id " +
+             "Inner Join departamento On departamento.id = elementoTI_departamento.fk_departamento " +
+             "Inner Join persona_elementoTI on persona_elementoTI.fk_elementoTI = elementoTI.id " +
+            "Inner Join persona on persona.id = persona_elementoTI.fk_persona " +
+             "Inner Join tipos_elementoTI on elementoTI.tipo = tipos_elementoTI.id Where tipos_elementoTI.id = elementoTI.tipo";
 
             var comando = new SqlConnection("Server=.\\SQLEXPRESS; Database= Sistema_Incidencias; Integrated Security=True"); // Your Connection String here
             var dataAdapter = new SqlDataAdapter(select, comando);
--- Sistema_Incidencias/Sistema_Incidencias/FormVerSoluciones.Designer.cs
+++ Sistema_Incidencias/Sistema_Incidencias/FormVerSoluciones.Designer.cs
@@ -35,13 +35,13 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(558, 252);
+            this.dataGridView1.Size = new System.Drawing.Size(746, 252);
             this.dataGridView1.TabIndex = 0;
             // 
             // button2
@@ -61,7 +61,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(579, 376);
+            this.ClientSize = new System.Drawing.Size(770, 376);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FormVerSoluciones";
